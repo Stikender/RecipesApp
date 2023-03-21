2b7abec1eb7c31f8948fcb86186a1582e078d85f
Stikender <nadik17@bk.ru>
2023-03-21 14:14:16 +0800

    Finish commit library

--- src/main/java/me/stremyakvann/recipesapp/controllers/IngredientController.java
+++ src/main/java/me/stremyakvann/recipesapp/controllers/IngredientController.java
@@ -1,5 +1,7 @@
 package me.stremyakvann.recipesapp.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import me.stremyakvann.recipesapp.dto.IngredientDTO;
 import me.stremyakvann.recipesapp.model.Ingredient;
 import me.stremyakvann.recipesapp.services.impl.IngredientsServiceImpl;
@@ -9,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/ingredient")
+@Tag(name = "Ингридиенты", description = "CRUD- ендпоинты для работы с ингридиентами")
 public class IngredientController {
 
     private final IngredientsServiceImpl ingredientsServiceImpl;
@@ -18,27 +21,51 @@ public class IngredientController {
     }
 
     @GetMapping
+    @Operation(
+            summary = "Поиск всех ингридиентов",
+            description = "Поиск осуществляется без заданных параметров"
+    )
     public List<IngredientDTO> getAllIngredient() {
         return ingredientsServiceImpl.getAllIngredient();
     }
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Поиск ингридиента по id(номеру)",
+            description = "Поиск осуществляется по параметру id(номер)"
+    )
     public IngredientDTO getIngredient(@PathVariable("id") int id){
         return ingredientsServiceImpl.getIngredient(id);
     }
     @PostMapping
+    @Operation(
+            summary = "Добавление ингридиента",
+            description = "Добавление осуществляется по заданным параметрам"
+    )
     public IngredientDTO addIngredient(@RequestBody Ingredient ingredient){
         return ingredientsServiceImpl.addIngredient(ingredient);
     }
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Редактирование ингридиента",
+            description = "Возможно редактирование любого заданного параметра"
+    )
     public IngredientDTO editIngredient(@PathVariable("id") int id, @RequestBody Ingredient ingredient) {
         return ingredientsServiceImpl.editIngredient(id, ingredient);
     }
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Удаление ингридиента",
+            description = "Поиск и удаление осуществляется по id(номеру)"
+    )
     public IngredientDTO deleteIngredient(@PathVariable("id") int id) {
         return ingredientsServiceImpl.deleteIngredient(id);
     }
 
     @DeleteMapping
+    @Operation(
+            summary = "Удаление всех ингридиентов",
+            description = "Поиск и удаление осуществляется без параметров"
+    )
     public void deleteAllIngredient() {
         ingredientsServiceImpl.deleteAllIngredient();
     }
--- src/main/java/me/stremyakvann/recipesapp/controllers/RecipeController.java
+++ src/main/java/me/stremyakvann/recipesapp/controllers/RecipeController.java
@@ -1,5 +1,7 @@
 package me.stremyakvann.recipesapp.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import me.stremyakvann.recipesapp.dto.RecipeDTO;
 import me.stremyakvann.recipesapp.model.Recipe;
 import me.stremyakvann.recipesapp.services.impl.RecipesServiceImpl;
@@ -9,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/recipe")
+@Tag(name = "Рецепты", description = "CRUD- ендпоинты для работы с рецептами")
 public class RecipeController {
 
     private final RecipesServiceImpl recipesServiceImpl;
@@ -18,29 +21,53 @@ public class RecipeController {
     }
 
     @GetMapping
+    @Operation(
+            summary = "Поиск всех рецептов",
+            description = "Поиск осуществляется без заданных параметров"
+    )
     public List<RecipeDTO> getAllRecipes() {
         return recipesServiceImpl.getAllRecipes();    }
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Поиск рецепта по id(номеру)",
+            description = "Поиск осуществляется по параметру id(номер)"
+    )
     public RecipeDTO getRecipe(@PathVariable("id") int id) {
         return recipesServiceImpl.getRecipe(id);
     }
 
     @PostMapping
+    @Operation(
+            summary = "Добавление рецепта",
+            description = "Добавление осуществляется по заданным параметрам"
+    )
     public RecipeDTO addRecipe(@RequestBody Recipe recipe) {
         return recipesServiceImpl.addRecipe(recipe);
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Редактирование рецепта",
+            description = "Возможно редактирование любого заданного параметра"
+    )
     public RecipeDTO editRecipe(@PathVariable("id") int id, @RequestBody Recipe recipe) {
         return recipesServiceImpl.editRecipe(id,recipe);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Удаление рецепта",
+            description = "Поиск и удаление осуществляется по id(номеру)"
+    )
     public RecipeDTO deleteRecipe(@PathVariable("id") int id) {
         return recipesServiceImpl.deleteRecipe(id);
     }
 
     @DeleteMapping
+    @Operation(
+            summary = "Удаление всех рецептов",
+            description = "Поиск и удаление осуществляется без параметров"
+    )
     public void deleteAllRecipe() {
     recipesServiceImpl.deleteAllRecipe();
     }
